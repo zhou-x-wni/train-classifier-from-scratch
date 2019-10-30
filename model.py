fb7c81eb3c7b5ee6ac381bfe6ba301ad817d0156
zhou-x-wni <zhou-x@wni.com>
2019-10-30 15:51:09 +0900

    Change tf version

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import numpy as np
-import tensorflow as tf
+#import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import matplotlib.pyplot as plt
 import data_processing
 
